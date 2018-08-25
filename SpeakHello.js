590522a6e613452c0df5158aa5a93856bd1f25e1
truboprovod4uk <truboprovod4uk@gmail.com>
2018-08-25 21:31:05 +0300

    first commit

---
+++ SpeakHello.js
@@ -0,0 +1,10 @@
+(function (){
+var speakWord = "Hello";
+var helloSpeaker = {};
+
+helloSpeaker.speak =  function (name) {
+  console.log(speakWord + " " + name);
+};
+
+window.helloSpeaker = helloSpeaker;
+})();
